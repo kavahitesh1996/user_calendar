0f5ab9a4cbe5d4af2e5e2a12d84a480cebce182a
root <root@lms.devstack.edx>
2020-11-05 09:34:40 +0000

    [REMOVE] edx-calendar-tab-google-api-private-key.json file dependency.

--- calendar_tab/utils.py
+++ calendar_tab/utils.py
@@ -1,6 +1,7 @@
 """
 Google Calendar API utils.
 """
+import os
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 
@@ -11,10 +12,10 @@ scopes = ['https://www.googleapis.com/auth/calendar']
 
 try:
     credentials = ServiceAccountCredentials.from_json_keyfile_name(
-        settings.FEATURES['GOOGLE_CALENDAR_TAB_PRIVATE_KEY_URL'], scopes)
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), 'edx-calendar-tab-google-api-private-key.json'), scopes)
 except (IOError, KeyError) as e:
     raise ImproperlyConfigured(
-        "You must set `FEATURES['GOOGLE_CALENDAR_TAB_PRIVATE_KEY_URL']` when "
+        "You must set edx-calendar-tab-google-api-private-key.json ` when "
         "`FEATURES['ENABLE_CALENDAR']` is True."
     )
 
